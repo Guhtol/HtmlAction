dbc2b01674dfcdf76511a5dbc6cdf4f335fd64a2
Guhtol <bueno_oab@hotmail.com>
2015-09-16 00:16:56 -0300

    First commit

---
+++ AprendaHtmlAction/App_Start/BundleConfig.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace AprendaHtmlAction.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle(
+                "~/bundles/jquery").Include(
+                "~/Scripts/jquery={version}.js", "~/Scripts/JavaScript.js"
+                ));
+            bundles.Add(new StyleBundle("~/bundles/Boots").Include
+                (
+                "~/Content/bootstrap.css"
+                ));
+        }
+    }
+}
---
+++ AprendaHtmlAction/Controllers/UsuariosController.cs
@@ -0,0 +1,42 @@
+﻿using AprendaHtmlAction.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AprendaHtmlAction.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        // GET: Usuarios
+        public ActionResult Inicio()
+        {
+            Professor pro = new Professor
+            {
+                Codigo=1,
+                Nome="Professor"
+            };
+            return View(pro);
+        }
+        public ActionResult _Aluno(int codigo,string nome)
+        {
+            Aluno aluno = new Aluno{
+                Codigo=codigo,
+                Nome=nome
+            };
+            return View(aluno);
+
+        }
+        public ActionResult _Heroi()
+        {
+            Heroi heroi = new Heroi
+            {
+                Codigo=1,
+                Nome="Otavio",
+                SuperPoder="Desenvolvedor"
+            };
+            return View(heroi);
+        }
+    }
+}
